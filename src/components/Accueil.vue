e35859278db5072557c6df1f7bc550de7a01939f
« Nicolas <n_kloc@etu-webschoolfactory.fr>
2022-07-11 11:49:54 +0200

    add: menu burger fini

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="home">
-    <Burger />
+  <div class="accueil">
+    
   </div>
 </template>
 
 <script>
-import Burger from '@/components/Burger.vue'
 export default {
   name: 'Accueil',
-  components: {
-    Burger
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-.home {
+.accueil {
   background: red
   height 1500px
 
